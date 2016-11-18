b8b9277c151c5623250c70d14440d3df343e81a3
franspaco <franspaco@gmail.com>
2016-11-18 13:55:14 -0600

    First commit

---
+++ index.php
@@ -0,0 +1,119 @@
+<?php
+  $_config = parse_ini_file("../php_files/config.php.ini");
+  $mysqli = new mysqli("localhost", $_config['mysqluser'], $_config['mysqlpass'], $_config['mysqlDB']);
+  $_ch = "all";
+ ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="A front-end template that helps you build fast, modern mobile web apps.">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+    <title> <?php echo $_ch ?> | chirper </title>
+
+    <!-- Add to homescreen for Chrome on Android -->
+    <meta name="mobile-web-app-capable" content="yes">
+    <link rel="icon" sizes="192x192" href="images/favicon.png">
+
+    <!-- Add to homescreen for Safari on iOS -->
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="apple-mobile-web-app-title" content="Material Design Lite">
+    <link rel="apple-touch-icon-precomposed" href="images/ios-desktop.png">
+
+    <!-- Tile icon for Win8 (144x144 + tile color) -->
+    <meta name="msapplication-TileImage" content="images/touch/ms-touch-icon-144x144-precomposed.png">
+    <meta name="msapplication-TileColor" content="#3372DF">
+
+    <link rel="shortcut icon" href="images/favicon.png">
+
+    <!-- SEO: If your mobile URL is different from the desktop URL, add a canonical link to the desktop page https://developers.google.com/webmasters/smartphone-sites/feature-phones -->
+    <!--
+    <link rel="canonical" href="http://www.example.com/">
+    -->
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="css/material.min.css" />
+    <link rel="stylesheet" href="css/custom.css" />
+    <link rel="stylesheet" href="css/hoover.css" />
+    <link rel="stylesheet" href="css/styles.css">
+    <style>
+    #view-source {
+      position: fixed;
+      display: block;
+      right: 0;
+      bottom: 0;
+      margin-right: 40px;
+      margin-bottom: 40px;
+      z-index: 900;
+    }
+    </style>
+  </head>
+  <body class="mdl-demo mdl-color--grey-100 mdl-color-text--grey-700 mdl-base">
+    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+      <header class="mdl-layout__header mdl-layout__header--scroll mdl-color--primary">
+        <div class="mdl-layout--large-screen-only mdl-layout__header-row">
+        </div>
+        <div class="mdl-layout--large-screen-only mdl-layout__header-row">
+          <h3>Name &amp; Title</h3>
+        </div>
+        <div class="mdl-layout--large-screen-only mdl-layout__header-row">
+        </div>
+        <div class="mdl-layout__tab-bar mdl-js-ripple-effect mdl-color--primary-dark">
+          <?php
+          $subs_qry = "SELECT * FROM CHANNELS WHERE is_default = true";
+          $subs_st = $mysqli->query($subs_qry);
+          $subs_st->data_seek(0);
+          echo "<a href=\"channel.php?ch=" . $_ch . "\" class=\"mdl-layout__tab\" id=\"current-channel\">#" . $_ch . "</a>";
+          while($row = $subs_st->fetch_assoc()){
+            echo "<a href=\"channel.php?ch=" . $row['name'] . "\" class=\"mdl-layout__tab\">#". $row['name'] . "</a>";
+          }
+          ?>
+        </div>
+      </header>
+      <main class="mdl-layout__content">
+        <div class="mdl-layout__tab-panel is-active" id="overview">
+          <section class="section--center mdl-grid mdl-grid--no-spacing">
+            <?php
+            $qry =
+              "SELECT USERS.username, POSTS.id, POSTS.message, POSTS.timestamp, CHANNELS.name
+                FROM (CHANNELS INNER JOIN POSTS
+	                 ON POSTS.channel = CHANNELS.id ) INNER JOIN USERS
+		                 ON USERS.id = POSTS.owner
+                     WHERE CHANNELS.name = '" . $mysqli->escape_string($_ch) . "'
+                     ORDER BY timestamp DESC";
+            //echo $qry;
+            $stmt = $mysqli->query($qry);
+            $stmt->data_seek(0);
+
+            while($row = $stmt->fetch_assoc()){
+              echo "<div class=\"mdl-card mdl-cell mdl-cell--12-col mdl-shadow--2dp post-card\">
+                      <div class=\"post-card-text mdl-card__supporting-text\">"
+                        . $row['message'] .
+                      "</div>
+                      <div class=\"mdl-card__actions\">
+                      <div class=\"author-tag\">
+                        by
+                        <a class=\"hvr-underline-reveal author-name\" href=\"user.php?="
+                          . $row['username'] . "\">"
+                          . $row['username'] .
+                        "</a>
+                        on "
+                        . $row['timestamp'] .
+                      "</div>
+                      <!--<div class=\"mdl-card__actions\">-->
+                        <a href=\"post.php?=". $row['id'] ."\" class=\"mdl-button\">LIKE</a>
+                        <a href=\"post.php?=". $row['id'] ."\" class=\"mdl-button\">Permalink</a>
+                      </div>
+                    </div>";
+            }
+            ?>
+          </section>
+        </div>
+      </main>
+    </div>
+    <script src="https://code.getmdl.io/1.2.1/material.min.js"></script>
+  </body>
+</html>
